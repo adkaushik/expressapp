860a3a7208a2591d3e49f04aeadaafee3832654e
A D Kaushik <adkaushik25@gmail.com>
2017-07-21 21:52:14 +0530

    new landing page

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
+const path = require('path');
+const port = 1795;
 
 var express = require('express')
 var app = express()
 
-app.get('/kaushik', function (req, res) {
-   res.sendFile(process.cwd()+'/index.html')
+app.get('/', function (req, res) {
+   res.sendFile(process.cwd() + '/index.html')
 });
 
+app.get('/app/style.css', function (req, res) {
+	res.sendFile(path.join(process.cwd(), 'app', 'style.css'))
+});
+
+app.get('/app/app.js', function (req, res) {
+	res.sendFile(path.join(process.cwd(), 'app', 'app.js'))
+});
 
 app.get('*', function (req, res) {
    res.send('Welcome to my app')
 });
 
-
-app.listen(process.env.PORT || 1795);
+console.log('server is listening on port: ', port)
+app.listen(process.env.PORT || port);
