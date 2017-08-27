55f4d39195011d2a210f1cd8a7cd293aa432937f
A D Kaushik <adkaushik25@gmail.com>
2017-08-27 19:18:00 +0530

    refactor the whole website

--- index.js
+++ index.js
@@ -4,21 +4,16 @@ const port = 1795;
 var express = require('express')
 var app = express()
 
+app.use(express.static('app'));
+
 app.get('/', function (req, res) {
    res.sendFile(process.cwd() + '/index.html')
-});
-
-app.get('/app/style.css', function (req, res) {
-	res.sendFile(path.join(process.cwd(), 'app', 'style.css'))
-});
-
-app.get('/app/app.js', function (req, res) {
-	res.sendFile(path.join(process.cwd(), 'app', 'app.js'))
 });
 
 app.get('*', function (req, res) {
    res.send('Welcome to my app')
 });
 
-console.log('server is listening on port: ', port)
 app.listen(process.env.PORT || port);
+
+console.log('server is listening on port: ', port);
